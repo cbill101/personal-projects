43b2319b8bc331ffe8ed853c8db683ca5a6550cf
Chris Billingsley <chris@localhost.localdomain>
2019-03-28 17:54:25 -0600

    Added rank assign functionality, still things to work out. And music is way more difficult than I thought it would be.

--- Go/DiscordBot/main.go
+++ Go/DiscordBot/main.go
@@ -11,6 +11,7 @@ import (
 var (
 	commandPrefix string
 	botID         string
+	voiceConn     *discordgo.VoiceConnection
 )
 
 func main() {
@@ -46,7 +47,6 @@ func main() {
 	commandPrefix = "!"
 
 	<-make(chan struct{})
-
 }
 
 /*
@@ -68,6 +68,30 @@ func findUserVoiceState(session *discordgo.Session, userid string) (*discordgo.V
 		}
 	}
 	return nil, errors.New("Could not find user's voice state")
+}
+
+func leaveVoiceChannel(vc *discordgo.VoiceConnection) (*discordgo.VoiceConnection, error) {
+	err := vc.Disconnect()
+
+	if err != nil {
+		vc.Close()
+		return nil, err
+	}
+
+	vc.Close()
+	vc = nil
+
+	return vc, nil
+}
+
+func AddRole(discord *discordgo.Session, gu *discordgo.Guild, role *discordgo.Role, user *discordgo.User) error {
+	err := discord.GuildMemberRoleAdd(gu.ID, user.ID, role.ID)
+
+	if err == nil {
+		return nil
+	}
+
+	return err
 }
 
 /*
@@ -106,27 +130,59 @@ func commandHandler(discord *discordgo.Session, message *discordgo.MessageCreate
 		}
 	case "!listen":
 		// Listen to music! (TODO)
-		if len(args) == 1 {
+		if len(args) >= 1 {
 			discord.ChannelMessageSend(message.ChannelID, "Proper usage... watch this space.")
-			vc, err := discord.ChannelVoiceJoin(message.GuildID, "560880394262806528", false, false)
-			errCheck("Couldn't join voice server", err)
-			vc.AddHandler(func(vc *discordgo.VoiceConnection, vs *discordgo.VoiceSpeakingUpdate) {
 
-			})
-
-			vc.Disconnect()
-			vc.Close()
 		} else {
 			discord.ChannelMessageSend(message.ChannelID, "Usage: !listen <url>")
 		}
 
 	case "!join":
-		if len(args) == 1 {
-			vs, err := findUserVoiceState(discord, message.Author.ID)
-			errCheck("Could not find user in voice channel.", err)
-			discord.ChannelVoiceJoin(vs.GuildID, vs.ChannelID, false, true)
+		vs, err := findUserVoiceState(discord, message.Author.ID)
+		errCheck("Could not find user in voice channel.", err)
+		if voiceConn != nil {
+			voiceConn.Disconnect()
+			voiceConn.Close()
 		}
 
+		voiceConn, err = discord.ChannelVoiceJoin(vs.GuildID, vs.ChannelID, false, true)
+		errCheck("Couldn't join voice channel.", err)
+		voiceConn.AddHandler(func(vc *discordgo.VoiceConnection, vs *discordgo.VoiceSpeakingUpdate) {
+
+		})
+
+	case "!leave":
+		leaveVoiceChannel(voiceConn)
+
+	case "!rank":
+		rolename := strings.Join(args, " ")
+		if strings.Compare("", rolename) == 0 {
+			// List ranks available.
+			break
+		}
+		gu, err := discord.Guild(message.GuildID)
+		errCheck("Couldn't get guild ID", err)
+		roles := gu.Roles
+		exists := false
+
+		for _, role := range roles {
+			if strings.Compare(role.Name, rolename) == 0 {
+				exists = true
+				err = AddRole(discord, gu, role, message.Author)
+				if err != nil {
+					discord.ChannelMessageSend(message.ChannelID, "Sorry "+message.Author.Mention()+", I could not assign the role **"+role.Name+"** to you.")
+				}
+				errCheck("Couldn't add role successfully", err)
+
+				discord.ChannelMessageSend(message.ChannelID, message.Author.Mention()+", you successfully joined **"+role.Name+"**.")
+				fmt.Println("Successfully added " + role.Name + " role to user " + message.Author.String())
+				break
+			}
+		}
+
+		if !exists {
+			discord.ChannelMessageSend(message.ChannelID, "The role **"+rolename+"** does not exist on this server.")
+		}
 	}
 
 	// Debugging server side, prints stuff
